5a774380564d7973a91ee41a30c514fa7f73e84f
kimyounghoon <chomil96@naver.com>
2024-07-19 22:25:42 +0900

    initialize near tile

--- MagicPang/Assets/Scripts/GameBoard.cs
+++ MagicPang/Assets/Scripts/GameBoard.cs
@@ -8,28 +8,102 @@ public class GameBoard : MonoBehaviour
 {
     private const int TileCntX = 8;
     private const int TileCntY = 8;
-    
+    private Dictionary<Vector2Int, Tile> tiles = new Dictionary<Vector2Int, Tile>();
+
+    public int movingTile = 0;
+    public List<Tile> swapTile = new List<Tile>();
+
+
     void Start()
     {
         GameManager.inst.curBoard = this;
 
 
-        for (int x = 0; x < TileCntX; x++)
+        for (int y = 0; y < TileCntY; y++)
         {
-            for (int y = 0; y < TileCntY; y++)
+            for (int x = 0; x < TileCntX; x++)
             {
-                int spawnPosX = 60 + 120 * x;
-                int spawnPosY = -60 - 120 * y;
-                int randomTileColor = Random.Range(0, (int)Tile_Color.None);
+                Vector2Int spawnPos = new Vector2Int(60 + 120 * x, -60 - 120 * y);
+                int randomTileColor = Random.Range(0, (int)Elemental.None);
 
                 Tile newTile = Instantiate(GameManager.inst.tilePrefabs[randomTileColor], transform);
-                newTile.transform.localPosition = new Vector3(spawnPosX, spawnPosY, 0);
+                newTile.transform.localPosition = new Vector3(spawnPos.x, spawnPos.y, 0);
+                newTile.tileIndex = new Vector2Int(x, y);
+                tiles.Add(newTile.tileIndex, newTile);
 
+                LinkingTile(newTile);
             }
         }
     }
 
     void Update()
     {
+        if (movingTile > 0)
+        {
+            return;
+        }
+    }
+
+    public void CheckMatch()
+    {
+    }
+
+
+    public void SwapTile(Tile tile1, Tile tile2)
+    {
+        if (tile1 == null || tile2 == null)
+        {
+            return;
+        }
+
+        (tile1.tileIndex, tile2.tileIndex) = (tile2.tileIndex, tile1.tileIndex);
+        (tiles[tile1.tileIndex], tiles[tile2.tileIndex]) = (tiles[tile2.tileIndex], tiles[tile1.tileIndex]);
+        LinkingTile(tile1);
+        LinkingTile(tile2);
+    }
+
+    public void LinkingTile(Tile tile)
+    {
+        Vector2Int tileIndex = tile.tileIndex;
+
+        if (tiles.TryGetValue(tileIndex + Vector2Int.down, out Tile upTile))
+        {
+            tile.nearTiles[0] = upTile; //U
+            upTile.nearTiles[1] = tile;
+        }
+        else
+        {
+            tile.nearTiles[0] = null;
+        }
+
+        if (tiles.TryGetValue(tileIndex + Vector2Int.up, out Tile downTile))
+        {
+            tile.nearTiles[1] = downTile; //D
+            downTile.nearTiles[0] = tile;
+        }
+        else
+        {
+            tile.nearTiles[1] = null;
+        }
+
+        if (tiles.TryGetValue(tileIndex + Vector2Int.left, out Tile leftTile))
+        {
+            tile.nearTiles[2] = leftTile; //L
+            leftTile.nearTiles[3] = tile;
+        }
+        else
+        {
+            tile.nearTiles[2] = null;
+        }
+
+        if (tiles.TryGetValue(tileIndex + Vector2Int.right, out Tile rightTile))
+        {
+            tile.nearTiles[3] = rightTile; //R
+            rightTile.nearTiles[2] = tile;
+        }
+        else
+        {
+            tile.nearTiles[3] = null;
+        }
     }
 }
--- MagicPang/Assets/Scripts/Tile.cs
+++ MagicPang/Assets/Scripts/Tile.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using Unity.VisualScripting;
 
-public enum Tile_Color
+public enum Elemental
 {
-    Red, Green, Blue, Yellow, Purple, None
+    Fire, Grass, Ground, Electric, Water, None
 }
 
 
-public class Tile : MonoBehaviour
+public enum Direction
 {
-    public Tile_Color color;
+    U, D, L, R, None
+}
+
+
+public class Tile : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    public bool isClicked = false;
+    public bool isMoving = false;
+    public bool isMatched = false;
+    public Elemental elemental;
+    public Vector2Int tileIndex;
+    public Tile[] nearTiles = new Tile[4]; //U,D,L,R;
     
     void Start()
     {
@@ -21,4 +36,53 @@ public class Tile : MonoBehaviour
     {
         
     }
+    private void Falling()
+    {
+    }
+    
+    
+    private void Poping()
+    {
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (GameManager.inst.curBoard.movingTile > 0)
+        {
+            return;
+        }
+        
+        isClicked = true;
+        Debug.Log("Button Pressed");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (GameManager.inst.curBoard.movingTile > 0)
+        {
+            return;
+        }
+        
+        isClicked = false;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (GameManager.inst.curBoard.movingTile > 0)
+        {
+            return;
+        }
+        if (isClicked == true)
+        {
+            Vector2 dir = eventData.position - new Vector2(transform.position.x,transform.position.y);
+            if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x))
+            {
+                Debug.Log(dir.y > 0 ? $"Button Exit U" : $"Button Exit D");
+            }
+            else
+            {
+                Debug.Log(dir.x > 0 ? $"Button Exit R" : $"Button Exit L");
+            }
+            isClicked = false;
+        }
+    }
+
 }
